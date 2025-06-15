6a9187010c57e54937e0125ac6caab17b14022ce
q-abe <abe@q-co.jp>
2025-06-16 00:28:43 +0900

    fixed vercel config in vite config

--- remix.config.js
+++
@@ -1,8 +0,0 @@
-const { vercel } = require('@vercel/remix');
-
-module.exports = vercel({
-  // Specify that we're using Vite as the build tool
-  buildCommand: "remix vite:build",
-  devCommand: "remix vite:dev",
-  serverBuildPath: "build/server/index.js",
-});
